d7d5d23c8cc98ee8b34e8828e8df442cfcbd26a6
matsu <matsuaki171101@gmail.com>
2023-06-27 14:12:00 +0900

    draft.jsによるglobalへのアクセスをviteconfigへと記述することで、エラー解消

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -3,10 +3,10 @@ import { Editor, EditorState } from 'draft-js'
 import 'draft-js/dist/Draft.css'
 
 export const TextEditor = () => {
-    // const [editorState, setEditorState] = React.useState(() =>
-    //     EditorState.createEmpty()
-    // );
+    const [editorState, setEditorState] = React.useState(() =>
+        EditorState.createEmpty()
+    );
 
-    // return <Editor editorState={editorState} onChange={setEditorState} />;
-    return <div>TextEditor</div>
+    return <Editor editorState={editorState} onChange={setEditorState} />;
+    // return <div>TextEditor</div>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // globalとwindowの互換性を示すことによってdraft.jsのエラーを解消
+  define: { global: 'window' }
 })
