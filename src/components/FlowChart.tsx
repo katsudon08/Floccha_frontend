6107a5ab926072c859a0d34c8dc02ce3ce05ce84
matsu <matsuaki171101@gmail.com>
2023-07-10 09:51:52 +0900

    initialのedgeとnodeのundefinedを許容して、フローチャート画面の初期状態を作成

--- src/components/FlowChart.tsx
+++ src/components/FlowChart.tsx
@@ -15,16 +15,21 @@ import ReactFlow, {
     addEdge,
     Edge,
     Connection,
+    Node,
 } from 'reactflow'
 import "reactflow/dist/style.css"
 import { Container, Box } from "@mui/material";
 
-const initialNodes = [
-    { id: "1", position: { x: 0, y: 0 }, data: { label: "最初" } },
-    { id: "2", position: { x: 0, y: 100 }, data: { label: "終わり" } },
+const initialNodes: Node<undefined | { label: string }>[] = [
+    // { id: "node-1", position: { x: 0, y: 0 }, data: { label: "node1" } },
+    // { id: "node-2", position: { x: 0, y: 100 }, data: { label: "node2" } },
+    // { id: "node-3", position: { x: 200, y: 100 }, data: { label: "node3" } },
 ];
 
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges: Edge<undefined>[] = [
+    // { id: "edge-1", source: "node-1", target: "node-2" },
+    // { id: "edge-2", source: "node-1", target: "node-3" },
+];
 
 export const FlowChart = () => {
     const [nodes, setnodes, onNodesChange] = useNodesState(initialNodes);
@@ -36,9 +41,9 @@ export const FlowChart = () => {
 
     return (
         <ReactFlow
-            // nodes={nodes}
+            nodes={nodes}
             // nodes={initialNodes}
-            // edges={edges}
+            edges={edges}
             // edges={initialEdges}
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
